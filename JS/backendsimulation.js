4dbb045ab0a7270d69ac1c20b7c363a06b7776b9
sashank6 <sashank.alladi@live.com>
2015-11-06 15:46:06 -0600

    js change

--- JS/backendsimulation.js
+++ JS/backendsimulation.js
@@ -1,7 +1,7 @@
 var studysession=[]
 var searchlist=[]
 var default_data = true
-
+var post={group:false,minsize:0,maxsize:0,partner:false,fromhour:0,fromminute:0,tohour:0,tominute:0,homework:false,examstudy:false,lecture_review:false,notes:false,other:false,subject:"",description:"",place:"",recurrence:0}
 //profile global
 var name = "Cheng Lin"
 var university = "Washington University in St. Louis"
@@ -40,7 +40,7 @@ function init()
 	//document.getElementById("PF_name").innerHTML = name;		
 	PF_university(false);
 	//document.getElementById("PF_university").innerHTML = university;		
-	var post={group:false,minsize:0,maxsize:0,partner:false,fromhour:0,fromminute:0,tohour:0,tominute:0,homework:false,examstudy:false,lecture_review:false,notes:false,other:false,subject:"",description:""}
+	
 	var ss1=Object.create(post);
 	var ss2=Object.create(post);
 	var ss3=Object.create(post);
