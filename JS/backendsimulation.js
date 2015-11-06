1c19081893cea8bf2dc8d6277e6fb636d21c6399
sashank6 <sashank.alladi@live.com>
2015-11-06 15:50:54 -0600

    js changes

--- JS/backendsimulation.js
+++ JS/backendsimulation.js
@@ -1,7 +1,7 @@
 var studysession=[]
 var searchlist=[]
 var default_data = true
-var post={group:false,minsize:0,maxsize:0,partner:false,fromhour:0,fromminute:0,tohour:0,tominute:0,homework:false,examstudy:false,lecture_review:false,notes:false,other:false,subject:"",description:"",place:"",recurrence:0}
+var post={group:false,minsize:0,maxsize:0,partner:false,fromhour:0,fromminute:0,tohour:0,tominute:0,homework:false,examstudy:false,lecture_review:false,notes:false,other:false,subject:"",description:"",place:"",recurrence:0,username:""}
 //profile global
 var name = "Cheng Lin"
 var university = "Washington University in St. Louis"
