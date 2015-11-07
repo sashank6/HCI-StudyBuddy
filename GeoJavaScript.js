eb9b130a0acf7c34f3ae7726fc8ba7682cee6086
geschw66 <geschw66@gmail.com>
2015-11-06 19:26:32 -0600

    added new  JS/InitPostStuddySession.js file and changed GeoJavaScript.js and post study session page

--- GeoJavaScript.js
+++ GeoJavaScript.js
@@ -10,30 +10,35 @@
         if (document.DemoForm.RadioGroup[i].checked) 
         {
             SelectedValue = document.DemoForm.RadioGroup[i].value;
+            alert("Radio Button: "+SelectedValue);
             break;
         }
     }
 
     if (document.getElementById('datepickr').value.toString() == "") {
 
-        alert("You must select a date!")
+        alert("You must select a date!");
         return;
     }
+
     strPrintOut = "<div style=\"background-color:#FFFF66;\"><p style=\"font-weight:bold;font-size:small;\">"+ document.getElementById('datepickr').value.toString() +"; @ 4:30PM: </p><ul><li>";
-    if (document.DemoForm.graduate.checked == true)
+    alert("DATE WORKS:" + document.getElementById('datepickr').value.toString());
+   
+    if (document.DemoForm.homework.checked == true)
       {
-          strPrintOut += document.DemoForm.graduate.value + " students</li><li>";
+          strPrintOut += document.DemoForm.homework.value + "</li><li>";
+          
        }
-      //alert(strPrintOut);
-      if (document.DemoForm.undergraduate.checked == true) {
+        
+      if (document.DemoForm.lecture_review.checked == true) {
 
-          strPrintOut += document.DemoForm.undergraduate.value + " students</li><li>";
+          strPrintOut += document.DemoForm.lecture_review.value + " students</li><li>";
        }
 
 
-       if (document.DemoForm.unspecified.checked == true) {
+       if (document.DemoForm.examstudy.checked == true) {
 
-           strPrintOut += document.DemoForm.unspecified.value + "</li><li>";
+           strPrintOut += document.DemoForm.examstudy.value + "</li><li>";
        }
 
        //Find selected text in selectOccurance ddl:
@@ -41,13 +46,14 @@
        var strFreq = e.options[e.selectedIndex].text;
        strPrintOut += strFreq + "</li><li>";
 
-       //alert(strPrintOut);
+       alert(strPrintOut);
        strPrintOut += SelectedValue + "</li></ul><br/></div><hr/>";
        document.getElementById("divOput").innerHTML += strPrintOut;
 
        document.DemoForm.ClickEnablePost.style.display = "block";
        document.DemoForm.ClickButton.style.display = "none";
-    
+
+       alert(strPrintOut);
     //document.DemoForm.Output.value = SelectedValue;
   }
 
---
+++ JS/InitPostStuddySession.js
@@ -0,0 +1,46 @@
+﻿function PostStudySession_Init() {
+    var usrverif = document.cookie;
+    if (usrverif == "")
+    window.location = "login.html";
+    var temparray = usrverif.split("=");
+    usernamecok = temparray[1];
+    name = usernamecok;
+}
+
+
+function requestsessions(i)
+ {
+     var ssPost = Object.create(post);
+
+     ssPost.group = 
+     ssPost.minsize=
+     ssPost.maxsizepartner=
+     ssPost.fromhour=
+     ssPost.fromminute=
+     ssPost.tohour=
+     ssPost.tominute=
+     ssPost.homework=
+     ssPost.examstudy=
+     ssPost.lecture_review=
+     ssPost.notes=
+     ssPost.other=
+     ssPost.subject=
+     ssPost.description=
+     ssPost.place=
+     ssPost.recurrence=
+     ssPost.username=
+
+    var request_sessions = localStorage.getItem(usernamecok + "_2");
+    if (request_sessions == null) {
+        request_sessions = [];
+        request_sessions.push(filsessions[i]);
+    }
+    else {
+        request_sessions = JSON.parse(request_sessions);
+        request_sessions.push(filsessions[i]);
+    }
+    var t = JSON.stringify(request_sessions);
+    localStorage.setItem(usernamecok + "_2", t);
+    displayrequest_sessions();
+
+}
