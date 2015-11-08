6ad425de91fbcc5dbed6b160734543d8932fed7e
geschw66 <geschw66@gmail.com>
2015-11-07 22:08:36 -0600

    Added more validation and calendar icon

--- JS/GeoJavaScript.js
+++ JS/GeoJavaScript.js
@@ -1,15 +1,21 @@
-﻿function Identify() {
+﻿
+/**
+* Submits data and puts it in the 3rd column.
+* */
+function submitData() {
     var SelectedValue;
     var i;
     var GroupSize;
     var strPrintOut;
 
-
+    
     //Go to validation function:
     if (postStudySessionValidation() == false) 
     {
         return false;
     }
+
+    
 
     //Group or Partner
     GroupSize = document.DemoForm.RadioGroup.length;
@@ -56,7 +62,8 @@
     // alert(strFromAmPm);
 
     //Creating a block of html and text to display the new entry:
-    strPrintOut = "<div style=\"background-color:#FFFF66;\"> <p style=\"font-weight:bold;font-size:small;\">" + document.DemoForm.subject.value.toString() + "</p>" +
+    strPrintOut = "<div style=\"background-color:#FFFF66;\"> <p style=\"font-weight:bold;font-size:small;\">" + document.DemoForm.subjectID.value.toString() + 
+    ": "+ document.DemoForm.subjectName.value.toString()+"</p>" +
     "<p style=\"font-weight:bold;font-size:small;\">" + document.getElementById('datepickr').value.toString() + "</p>" +
     "<p style=\"font-weight:bold;font-size:small;\"> FROM: " + document.DemoForm.FromHr.value.toString() + ":" + document.DemoForm.FromMin.value.toString() +
     " " + strFromAmPm + " - " + document.DemoForm.ToHr.value.toString() + ":" + document.DemoForm.ToMin.value.toString() +
@@ -94,21 +101,26 @@
 
    }
 
-
+   /**
+   * Makes the Submit button appear and the 
+   * "Post Another Session?" button disapear
+   *   also clears all fields for next entry.
+   **/
   function EnableClickButton() {
 
-      //Switch buttons:
-      document.DemoForm.ClickButton.style.display = "block"; //visible.
-      document.DemoForm.ClickEnablePost.style.display = "none"; //invisible.
-
+ 
       //Time:
       document.DemoForm.FromHr.value = "";
       document.DemoForm.FromMin.value = "";
       document.DemoForm.ToHr.value = "";
       document.DemoForm.ToMin.value = "";
 
+      //Location:
+      document.DemoForm.sessionlocation.value = "";
+
       //Subject, Date:
-      document.DemoForm.subject.value = "";
+      document.DemoForm.subjectID.value = "";
+      document.DemoForm.subjectName.value = "";
       document.DemoForm.datepickr.value = "";
 
       //Checkboxes:
@@ -118,36 +130,76 @@
       document.DemoForm.notes.checked = false;
       document.DemoForm.other.checked = false;
 
+      //Freq;
+      document.DemoForm
+
       //Group Min & Max:
       document.DemoForm.groupMin.value = 1;
       document.DemoForm.groupMax.value = 2;
+
+      //Reoccurance / Freq.
+      document.DemoForm.selectOccurance.selectedIndex = 0;
+      
 
       //Group Radio button:
       document.DemoForm.RadioGroup[1].checked = true;
       document.DemoForm.RadioGroup[0].checked = false;
       makeGroupDivInvisible();
+
+      //Session Notes:
+      document.DemoForm.sessionNotes.value = "";
+
+      //Switch buttons:
+      document.DemoForm.ClickButton.style.display = "block"; //visible.
+      document.DemoForm.ClickEnablePost.style.display = "none"; //invisible.
+
   }
 
+  /**
+  * Makes the Group min 
+  *  and max input fields appear.
+  **/
   function makeGroupDivVisible() {
 
-      document.getElementById('groupMinMaxDiv').style.display = "block";
-      
+      document.getElementById('groupMinMaxDiv').style.display = "block";     
 
   }
-
+  /**
+  * Makes the Group min and
+  *   max input fields disappear.
+  **/
   function makeGroupDivInvisible() {
 
       document.getElementById('groupMinMaxDiv').style.display = "none";
 
   }
 
+  /**
+  * Validates fields...most of them -- most
+  *    cases, well should be all of them.
+  **/
   function postStudySessionValidation() 
   {
 
       //Subject validation:
-      if (document.DemoForm.subject.value.toString() == "") {
+     var subID  = document.DemoForm.subjectID.value.trim();
+      if ( subID.toString() == "") {
+
+          alert("You must type a course ID!");
+          return false;
+      }
+
+     var subName  = document.DemoForm.subjectName.value.trim();
+      if ( subName.toString() == "") {
 
           alert("You must type a course name!");
+          return false;
+      }
+
+      var sessloc = document.DemoForm.sessionlocation.value.trim();
+      if (sessloc.toString() == "") {
+
+          alert("You must type a session location!");
           return false;
       }
 
@@ -195,5 +247,4 @@
      }
 
   }
-
 
