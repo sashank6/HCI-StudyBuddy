cea01fe34bd95206a5a73ca7aaff8d15bc945613
sashank6 <sashank.alladi@live.com>
2015-11-08 00:27:36 -0600

    completed registration validation

--- JS/login.js
+++ JS/login.js
@@ -31,6 +31,54 @@ function loginverify()
 		document.getElementById("errormessage").innerHTML="Username or password is invalid";
 	}
 }
+
+function registervalidate()
+{
+	var email=document.getElementById("email").value;
+	var username=document.getElementById("username1").value;
+	var password=document.getElementById("password1").value;
+	var confirmpassword=document.getElementById("confirmpassword").value;
+	var msg="";
+	if(validate_email(email))
+	{
+		if(usernamevalidate(username))
+		{
+			if(password==confirmpassword)
+			{
+				if(passwordvalidate(password))
+				{
+					window.location="verification.html";
+				}
+				else
+				{
+					msg="Password is invalid";
+				}
+			}
+			else
+			{
+				msg="Password's don't match";
+			}
+				
+		}
+		else
+		{
+			msg="username entered is invalid";
+		}
+	}
+	else
+	{
+		msg="Invalid email please input your wustl.edu email";
+	}
+	document.getElementById("register_error_message").innerHTML=msg;
+}
+
+function validate_email(email)
+{
+	var pattern=/(^[a-zA-Z]+[0-9_a-zA-Z]*)@wustl.edu/;
+	return pattern.test(email);
+}
+
+
 function usernamevalidate(username)
 {
 	var pattern=/^([a-zA-Z0-9]){4,10}$/;
@@ -43,68 +91,3 @@ function passwordvalidate(password)
 }
 
 
-function init()
-{
-	var hour_html="";
-	var minute_html="";
-	for(var i=0;i<24;i++)
-		hour_html+="<option>"+i+"</option>";
-	for(var i=0;i<60;i++)
-		minute_html+="<option>"+i+"</option>"
-	document.getElementById("from-hour").innerHTML=hour_html;
-	document.getElementById("to-hour").innerHTML=hour_html;
-	document.getElementById("from-minute").innerHTML=minute_html;
-	document.getElementById("to-minute").innerHTML=minute_html;
-	var post={group:false,minsize:0,maxsize:0,partner:false,fromhour:0,fromminute:0,tohour:0,tominute:0,homework:false,examstudy:false,lecture_review:false,notes:false,other:false}
-	
-	
-}
-
-
-function applyfilter()
-{
-	var group=false;
-	var minsize=0;
-	var maxsize=0;
-	if(document.getElementById("group").checked)
-	{
-		group=true;
-		minsize=document.getElementById("minsize").selectedIndex+1;
-		maxsize=document.getElementById("maxsize").selectedIndex+1;
-	}
-	var partner=document.getElementById("partner").checked;
-	//var date=document.getElementById("selectDate").checked;
-	var fromhour=document.getElementById("from-hour").selectedIndex;
-	var fromminute=document.getElementById("from-minute").selectedIndex;
-	var tohour=document.getElementById("to-hour").selectedIndex;
-	var tominute=document.getElementById("to-minute").selectedIndex;
-	var homework=document.getElementById("homework").checked;
-	var examstudy=document.getElementById("examstudy").checked;
-	var lecture_review=document.getElementById("lecture_review").checked;
-	var notes=document.getElementById("notes").checked;
-	var other=document.getElementById("other").checked;
-	alert(group+","+minsize+","+maxsize+","+partner+","+fromhour+","+fromminute+","+tohour+","+tominute+","+homework+","+examstudy+","+lecture_review+","+notes+","+other);
-	
-	searchresults();
-	
-	
-	
-	
-}
-function searchresults()
-{
-	
-}
-
-
-function onchangegroup()
-{
-	if(document.getElementById("group").checked)
-	{
-		document.getElementById("ifgroupchecked").innerHTML="Size: <select id=\"minsize\"><option>1</option><option>2</option><option>3</option><option>4</option></select>-<select id=\"maxsize\"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select>";
-	}
-	else
-	{
-		document.getElementById("ifgroupchecked").innerHTML="";
-	}
-}
