b6529bfacd55ab5aad5169f00d9d3c0e3a3e454f
adayoegi <adayoegi@hotmail.com>
2015-11-08 14:17:23 +0800

    Cheng
    pending request temp

--- JS/pending_requests.js
+++ JS/pending_requests.js
@@ -7,15 +7,36 @@ function init_pending_requests()
 	var temparray=usrverif.split("=");
 	usernamecok=temparray[1];
 	display_pendingrequests();
+	
 }
 
-
+/*
+{"username":"buddy",
+"post":{"
+subject":"CSE541","
+description":"Some Description","
+group":true,"
+minsize":2,"
+maxsize":5,"
+partner":false,"
+fromhour":13,"
+tohour":15,"
+fromminute":23,"
+tominute":29,"
+homework":false,"
+examstudy":true,"
+lecture_review":true,"
+notes":false,"
+other":false,"
+username":"buddy"}},
+*/
+//{"username":"sashank","post":{"subject":"CSE541","description":"Some Description","group":true,"minsize":2,"maxsize":5,"partner":false,"fromhour":13,"tohour":15,"fromminute":23,"tominute":29,"homework":false,"examstudy":true,"lecture_review":true,"notes":false,"other":false,"username":"buddy"}}]
 
 function display_pendingrequests()
 {
 	
 	var pending_requests=localStorage.getItem(usernamecok+"_9");
-	
+	//alert(pending_requests)
 	var temp="";
 	if(pending_requests==null)
 	{
@@ -25,8 +46,29 @@ function display_pendingrequests()
 	{
 		
 		pending_requests=JSON.parse(pending_requests);
-		
-		temp+=pending_requests[0].username+" "+pending_requests[0].post.subject;
+		for (i = 0; i < pending_requests.length; i++)
+		{
+			temp+="<h3 onclick=\"PR_detail("+i+")\">"+pending_requests[i].username+"</h3>is asking to join<h3>"+pending_requests[i].post.subject+"</h3></br>"+
+				"<input type =\"button\" value=\"accept\" id=\"PR_accept("+i+")\"></button>"
+		}
 	}
 	document.getElementById("request_info").innerHTML=temp;
+	
+}
+
+function PR_detail(i)
+{
+	alert("test")
+	var pending_requests=localStorage.getItem(usernamecok+"_9");
+	document.getElementById("pending_requests").innerHTML = "<h3>XDDDDD</h3>"
+}
+
+function PR_accept(i)
+{
+		alert(i)
+}
+
+function PR_deny(i)
+{
+	
 }
