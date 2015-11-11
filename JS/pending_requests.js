e1ad654a3708a9acfe7e5edb1d0dd235a662c2ab
adayoegi <adayoegi@hotmail.com>
2015-11-11 11:05:56 +0800

    Cheng
    pending button almost done

--- JS/pending_requests.js
+++ JS/pending_requests.js
@@ -46,13 +46,13 @@ function display_pendingrequests()
 	pending_requests=localStorage.getItem(usernamecok+"_9");
 	
 	
-	if(pending_requests==null)
+	if(pending_requests==null || pending_requests.length == 0)
 	{
 		temp="<p>You do not have any requests pending!</p>";
 	}
 	else
 	{
-		
+		alert("pend = "+pending_requests)
 		pending_requests=JSON.parse(pending_requests);
 		
 		for (i = 0; i < pending_requests.length; i++)
@@ -64,8 +64,8 @@ function display_pendingrequests()
 				"</br>To:"+pending_requests[i].post.tohour+":"+pending_requests[i].post.tominute+
 				"</br>Place:"+pending_requests[i].post.place+
 				"</br>Recurrence:"+pending_requests[i].post.recurrence+"</br>"+
-				"<input type=\"button\" value=\"Accept\" id=\"PR_accept\" onclick=\"PR_accept("+i+")\"></button>"+
-				"<input type=\"button\" value=\"Deny\" id=\"PR_deny\" onclick=\"PR_deny("+i+")\"></button>"
+				"<input type=\"button\" value=\"Accept\" id=\"PR_accept\" onclick=\"PR_accept("+i+",1)\"></button>"+
+				"<input type=\"button\" value=\"Deny\" id=\"PR_deny\" onclick=\"PR_accept("+i+")\",0></button>"
 		}
 	
 	}
@@ -80,7 +80,7 @@ function PR_detail(i)
 	document.getElementById("pending_requests").innerHTML = "<h3>XDDDDD</h3>"
 }
 
-function PR_accept(i)
+function PR_accept(i,decision)
 {
 	alert("accepted")
 	
@@ -109,38 +109,16 @@ function PR_accept(i)
 		applicant_sessions_applied[j].description == pending_requests[i].post.description
 		)
 		{
-			alert("3 in if"+applicant_sessions_applied[j].subject)
 			applicant_schedule_add=Object.create(request);
-			alert("3.1")
-			alert(applicant_schedule_add.post)
 			applicant_schedule_add.username = usernamecok
-			alert("3.2"+applicant_schedule_add.username)
 			applicant_schedule_add.post = applicant_sessions_applied[j]
-			/*
-			applicant_schedule_add.post.fromhour = pending_requests[i].post.fromhour
-			applicant_schedule_add.post.fromminute = pending_requests[i].post.fromminute
-			applicant_schedule_add.post.tohour = pending_requests[i].post.tohour
-			applicant_schedule_add.post.tominute = pending_requests[i].post.tominute
-			applicant_schedule_add.post.subject = pending_requests[i].post.subject
-			applicant_schedule_add.post.recurrence = pending_requests[i].post.recurrence
-			alert("3.5")
-			applicant_schedule_add.post.homework = pending_requests[i].post.homework
-			applicant_schedule_add.post.lecture_review = pending_requests[i].post.lecture_review
-			applicant_schedule_add.post.examstudy = pending_requests[i].post.examstudy
-			applicant_schedule_add.post.notes == pending_requests[i].post.notes
-			applicant_schedule_add.post.description = pending_requests[i].post.description
-			*/
-			alert("3.9")
 			
 			//applicant_schedule_add = applicant_sessions_applied.splice(j,1)
 			applicant_sessions_applied.splice(j,1)
-			alert("4"+applicant_schedule_add.post.subject)
 			applicant_scheduled_session.push(applicant_schedule_add)
-			alert("5"+applicant_scheduled_session[0].post.subject)
-			applicant_scheduled_session = JSON.stringify(applicant_scheduled_session)
-			alert("6"+applicant_scheduled_session[0])
-			localStorage.setItem(applicant_username+"_1", applicant_scheduled_session)
-			alert("applicant schedule session = "+applicant_schedule_add.post.subject)
+			if (applicant_scheduled_session.length != 0) applicant_scheduled_session = JSON.stringify(applicant_scheduled_session)
+			if (decision == 1) localStorage.setItem(applicant_username+"_1", applicant_scheduled_session)
+			
 			applicant_sessions_applied = JSON.stringify(applicant_sessions_applied)
 			localStorage.setItem(applicant_username+"_3", applicant_sessions_applied)
 			
@@ -157,11 +135,17 @@ function PR_accept(i)
 	*/
 	//localStorage.setItem(applicant_username+"_3",applicant_sessions_applied);
 	alert(applicant_username)
-	document.getElementById("PR_test_area").innerHTML=applicant_scheduled_session//applicant_sessions_applied[0].place
-	//applicant_scheduled_session = JSON.parse(localStorage.getItem(applicant_username+"_1"));
-	document.getElementById("PR_test_area").innerHTML=applicant_scheduled_session[0].post.subject//applicant_sessions_applied[0].place
 	
-	PR_delete(i)
+	//PR_delete(i)
+	pending_requests.splice(i,1)
+	alert("d1")
+	pending_requests = JSON.stringify(pending_requests)
+	alert("d2")
+	localStorage.setItem(usernamecok+"_9", pending_requests)
+	alert("d3")
+	display_pendingrequests()
+	alert("d4")
+
 }
 
 function PR_deny(i)
